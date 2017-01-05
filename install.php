9b0423f38754ca5f5a5dc22aae5e0111ba06cc04
Andersom <dev.andersom@aticonsultoria.com>
2017-01-05 10:33:48 -0200

    try catch no alter table _product

--- install.php
+++ install.php
@@ -3,5 +3,6 @@
 $db = $this->db;
 
 // Adiciona a coluna 'manufacturing_deadline' na tabela *_product
-$db->query("ALTER TABLE " . DB_PREFIX . "product ADD manufacturing_deadline INT(11) DEFAULT '0' NOT NULL AFTER stock_status_id");
-
+try {
+    $db->query("ALTER TABLE " . DB_PREFIX . "product ADD manufacturing_deadline INT(11) DEFAULT '0' NOT NULL AFTER stock_status_id");
+} catch (Exception $exception) {}
